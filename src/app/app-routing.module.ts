859c17b1141d52c607e7510858b26f0fe6b77fe6
alphamen01 <lesg.2018@hotmail.com>
2023-07-31 13:12:33 -0500

    se configuro el ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Componentes
+import { ListadoCursosComponent } from './components/listado-cursos/listado-cursos.component';
+import { AgregarEditarCursosComponent } from './components/agregar-editar-cursos/agregar-editar-cursos.component';
+import { VerCursosComponent } from './components/ver-cursos/ver-cursos.component';
+
+const routes: Routes = [
+  {path: '',redirectTo:'Cursos', pathMatch:'full'},
+  {path: 'Cursos',component: ListadoCursosComponent},
+  {path: 'AgregarCurso',component:  AgregarEditarCursosComponent},
+  {path: 'VerCurso/:Id',component:  VerCursosComponent},
+  {path: 'EditarCurso/:Id',component:  AgregarEditarCursosComponent},
+  {path: '**',redirectTo:'Cursos', pathMatch:'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
